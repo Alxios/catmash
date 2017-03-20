3b7a4992fb2d66e5ebc1a680bb8da072680d9164
dragon_ <alexgaspar@hotmail.fr>
2017-03-20 15:29:36 +0100

    brand compoment && logo

--- src/components/Brand.vue
+++ src/components/Brand.vue
@@ -1,14 +1,34 @@
 <template>
-
+  <div class="brand">
+    <div class="title">
+      <img class="logo" src="../assets/logo.png" alt="logo">
+    </div>
+    <p class="secondary-text center">{{ subTitle }}</p>
+  </div>
 </template>
 
 <script>
 export default {
   name: 'brand',
   data () {
-    return {}
+    return {
+      subTitle: 'Which will be your favorite?'
+    }
   }
 }
 </script>
 
+<style scoped>
+.brand {
+  padding: 25px 0 25px 0;
+}
+
+.title {
+  text-align: center;
+  font-size: 28px;
+}
+
+.title .logo {
+  padding-left: 70px;
+}
 </style>
